8f00ad3f94914cc581fce397b3b6a811e57ca9bb
Michal Mamica <michal.mamica@wp.pl>
2020-02-20 20:18:30 +0100

    Added tests and new interface BookRepository

--- src/main/java/pl/connectis/projektgrupowy/repository/BookRepository.java
+++ src/main/java/pl/connectis/projektgrupowy/repository/BookRepository.java
@@ -1,10 +1,10 @@
 package pl.connectis.projektgrupowy.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.connectis.projektgrupowy.domain.Book;
 
 @Repository
-public class BookRepository  {
-
-
+public interface BookRepository extends JpaRepository<Book, Integer> {
 
 }
